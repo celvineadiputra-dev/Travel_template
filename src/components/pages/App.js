36fa7149cc52ab70b04faa1c672eb58eb7e74833
celvine <celvineadiputra2@gmail.com>
2021-07-28 10:34:11 +0700

    Update Design pattern

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,15 +1,8 @@
 import Home from "./Home"
-import Navbar from "../ui/organisms/navbar"
 import '../../assets/scss/styles.scss'
-import "../../assets/scss/_home.scss"
 function App() {
   return (
-    <>
-      <div className="bg-decore">
-        <Navbar/>
-        <Home/>
-      </div>
-    </>
+    <Home/>
   );
 }
 
